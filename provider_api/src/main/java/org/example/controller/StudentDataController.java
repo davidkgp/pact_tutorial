17f6ecce7679ed6d4942edf5a3d961031fb15f25
Kaushik Bagchi <davidkgp@gmail.com>
2020-02-02 20:11:44 +0100

    changes for selflinks

--- provider_api/src/main/java/org/example/controller/StudentDataController.java
+++ provider_api/src/main/java/org/example/controller/StudentDataController.java
@@ -3,12 +3,12 @@ package org.example.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.controller.dto.Student;
 import org.example.service.StudentDataService;
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -30,7 +30,27 @@ public class StudentDataController {
 
         Student student = studentDataService.getStudent(rollId);
         student.add(linkTo(methodOn(StudentDataController.class).getStudent(rollId)).withSelfRel());
+        student.add(linkTo(methodOn(StudentDataController.class).deleteStudent(rollId)).withRel(LinkRelation.of(HttpMethod.DELETE.name())));
         return new ResponseEntity<>(student, HttpStatus.OK);
+
+
+    }
+
+    @PostMapping(path = "/student")
+    public HttpEntity<Student> createStudent(Student student) {
+
+        student.add(linkTo(methodOn(StudentDataController.class).getStudent(student.getRollId())).withSelfRel());
+        student.add(linkTo(methodOn(StudentDataController.class).deleteStudent(student.getRollId())).withRel(LinkRelation.of(HttpMethod.DELETE.name())));
+
+        return new ResponseEntity<>(student, HttpStatus.OK);
+
+
+    }
+
+    @DeleteMapping(path = "/student/{id}")
+    public HttpEntity<?> deleteStudent(@PathVariable("id") String rollId) {
+
+        return new ResponseEntity<>(HttpStatus.OK);
 
 
     }
